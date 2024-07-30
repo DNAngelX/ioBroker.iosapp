453756b714c727c271b19d9a5e53fcd236cb5bf4
ioBroker Adapter Creator <noreply@iobroker.net>
2024-07-30 22:03:51 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,19 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"iosapp adapter settings": {
+		"en": "Adapter settings for iosapp",
+		"de": "Adaptereinstellungen für iosapp",
+		"ru": "Настройки адаптера для iosapp",
+		"pt": "Configurações do adaptador para iosapp",
+		"nl": "Adapterinstellingen voor iosapp",
+		"fr": "Paramètres d'adaptateur pour iosapp",
+		"it": "Impostazioni dell'adattatore per iosapp",
+		"es": "Ajustes del adaptador para iosapp",
+		"pl": "Ustawienia adaptera dla iosapp",
+		"zh-cn": "iosapp的适配器设置"
+	}
+};
